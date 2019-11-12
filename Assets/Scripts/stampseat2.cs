74ca2c3cc8109239aeefc3e78cb35a7d2618de40
miri514 <57343003+miri514@users.noreply.github.com>
2019-11-12 18:38:34 +0900

    last

---
+++ Assets/Scripts/stampseat2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class stampseat2 : MonoBehaviour
+{
+    public GameObject stamp1;
+    public GameObject stamp2;
+    public GameObject stamp3;
+    public GameObject stamp4;
+    public GameObject stamp5;
+    public GameObject stamp6;
+    public GameObject stamp7;
+    public GameObject stamp8;
+    public GameObject stamp9;
+    public GameObject stamp10;
+    public GameObject stamp11;
+    public GameObject stamp12;
+    public GameObject stamp13;
+    public GameObject stamp14;
+    public GameObject stamp15;
+    public GameObject stamp16;
+    public GameObject stamp17;
+    public GameObject stamp18;
+    public GameObject stamp19;
+    public GameObject stamp20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*if(Test.SampleGoal == "代々木上原")
+        {
+            stamp1.SetActive(true);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
